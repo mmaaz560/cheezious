116cd14ff863df1f00dbc6f5a6025e9f83d9f984
mmaaz560 <maazibrahim560@gmail.com>
2026-02-07 07:41:38 -0800

    qwerfsfc

---
+++ abmin-dashbord.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './bootstrap.php'?>
+    <title>admin-dashboard</title>
+    <style>
+        body{
+            overflow: hidden;
+        }
+        li:hover{
+            background-color: var(--bs-warning);
+            border-radius: 10px;
+            cursor: pointer;
+
+        }
+    </style>
+</head>
+<body>
+    <div class="row">
+       <div class="col-xl-3 col-lg-3 col-md-4 shadow min-vh-100">
+        <h2 class="fw-bold text-center py-3 ">
+            Cheezymeal <i class="bi bi-circle-fill text-warning fs-6 py-0"></i></h2>
+        <ul class="list-unstyled px-5 fw-semibold">
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-speedometer2"></i>
+               Dashboard
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-basket"></i>
+               Food Order
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-heart"></i>
+               Favorite
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-clock-history"></i>
+               Order History
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-receipt"></i>
+               Bills
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-gear"></i>
+               Settings
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-tags"></i>
+               Categories
+           </li>
+           <li class="d-flex gap-3  p-3 align-items-center">
+               <i class="bi bi-egg-fried"></i>
+                Meals
+            </li>
+
+        </ul>
+
+       </div>
+       <div class="col-xl-9 col-lg-9 col-md-8 min-vh-100">
+        
+       </div>
+    </div>
+</body>
+</html>
